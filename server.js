7d63f1f92571a08fb88d2ec4c042acf82235f0c3
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-01-18 04:07:12 -0800

    setting up server and added git ignore

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Create Web App
+const server = express();
+
+// Set view engine for ejs
+server.set("view engine", "ejs");
+
+// Configure Middlewaer for parsing body, JSON, public folder
+server.use(express.static("public"));
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+
+// Configure .env file
+dotenv.config();
+
+// Global Error Handling
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something unusual occurred");
+});
+
+// Landing Page
+server.route("/")
+.get(async (req, res) => {
+  res.send("Hello World");
+});
+
+// Listen for server
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
