c9b9637480d482ad725ddf8f74f3a173436e4f70
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-02-22 18:32:19 -0800

    added function to add names to new db collection

--- db/database.js
+++ db/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import rsvp from "../models/reservations.js";
+import {rsvp, tradRsvp} from "../models/reservations.js";
 import dotenv from 'dotenv';
 
 // Configure .env file
@@ -27,6 +27,34 @@ export async function newRSVP(first_name, last_name) {
 
   try {
     const reserve = await rsvp.create({ 
+      firstName: newFirstName, 
+      lastName: newLastName,
+    });
+    
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+// Function to add new tradRSVP to database
+export async function newTradRSVP(first_name, last_name) {
+  
+  //Function to captialize first letter in both names
+  function upper(name) {
+
+    const capName = 
+      name.charAt(0).toUpperCase()
+      + name.slice(1);
+
+    // trim method to remove potential blank spaces in string
+    return capName.trim();
+  }
+
+  const newFirstName = upper(first_name);
+  const newLastName = upper(last_name);
+
+  try {
+    const reserve = await tradRsvp.create({ 
       firstName: newFirstName, 
       lastName: newLastName,
     });
